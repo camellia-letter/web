5fc2e87da87efee82872025caa1397b417f86cfd
rebi13 <rebi13@naver.com>
2026-06-13 14:22:25 +0900

    Fix(apps-web): Footer 배경색 미적용 버그 수정
      AppShellFooter를 Box 컴포넌트로 변경하여 배경색이 정상적으로 적용되도록 수정

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,20 +1,21 @@
-import { AppShell, AppShellFooter, Container, Text } from "@mantine/core";
-
-const footerStyle = {
-  position: 'static',
-  backgroundColor: '#111827',
-} as const;
+import { Box, Container, Text } from "@mantine/core";
 
 export const Footer = () => {
   return (
-    <AppShell mode="static">
-      <AppShellFooter py={48} px="md" withBorder={false} style={footerStyle}>
-        <Container size="xl">
-          <Text size="sm" ta="center" c="gray.4">
-            &copy; 2026 CamelliaLetter. All rights reserved.
-          </Text>
-        </Container>
-      </AppShellFooter>
-    </AppShell>
+    <Box
+      component="footer"
+      py={48}
+      px="md"
+      style={{
+        backgroundColor: '#111827',
+        width: '100%',
+      }}
+    >
+      <Container size="xl">
+        <Text size="sm" ta="center" c="gray.4">
+          &copy; 2026 CamelliaLetter. All rights reserved.
+        </Text>
+      </Container>
+    </Box>
   );
 };
